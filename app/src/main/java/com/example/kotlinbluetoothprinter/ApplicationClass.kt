d6278dbb3294108cc8494d2948e9b8f715832c4c
Carl <delacruzcarlangelo24@gmail.com>
2022-04-22 17:15:35 +0800

    Initial commit

---
+++ app/src/main/java/com/example/kotlinbluetoothprinter/ApplicationClass.kt
@@ -0,0 +1,12 @@
+package com.example.kotlinbluetoothprinter
+
+import android.app.Application
+import com.mazenrashed.printooth.Printooth
+
+class ApplicationClass : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Print
+    }
+}
